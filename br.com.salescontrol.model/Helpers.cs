cb4be11563ab6aef83ea5c41c39779629eb93a26
guialves202 <guimacielalves2@gmail.com>
2023-11-27 00:13:02 -0300

    :sparkles: feat(helpers): helpers class with clear screen method

---
+++ br.com.salescontrol.model/Helpers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace salesControl.br.com.salescontrol.model
+{
+    public class Helpers
+    {
+        public void ClearScreen(Form screen)
+        {
+            foreach (Control mainControl in screen.Controls)
+            {
+                foreach (Control subControl1 in mainControl.Controls)
+                {
+                    if (subControl1 is TabPage)
+                    {
+                        foreach (Control subControl2 in subControl1.Controls)
+                        {
+                            if (subControl2 is TextBox)
+                            {
+                                (subControl2 as TextBox).Clear();
+                            }
+
+                            if (subControl2 is MaskedTextBox)
+                            {
+                                (subControl2 as MaskedTextBox).Clear();
+                            }
+
+                            if (subControl2 is ComboBox)
+                            {
+                                (subControl2 as ComboBox).Text = "";
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
