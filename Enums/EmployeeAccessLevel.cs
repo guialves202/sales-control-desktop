d584ad0bfe9c7ed2cad2e71087589e1463968fc4
guialves202 <guimacielalves2@gmail.com>
2023-11-27 23:44:37 -0300

    :sparkles: feat(enum): employee access level enum

---
+++ Enums/EmployeeAccessLevel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace salesControl.Enums
+{
+    public enum EmployeeAccessLevel
+    {
+        Administrador = 0,
+        Usuario = 1
+    }
+}
