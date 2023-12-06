d499ae99edc1a2e8cadce310904962ce19ee2b0d
guialves202 <guimacielalves2@gmail.com>
2023-12-05 23:38:06 -0300

    :sparkles: feat(model): product model

---
+++ br.com.salescontrol.model/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace salesControl.br.com.salescontrol.model
+{
+    public class Product
+    {
+        public int code { get; set; }
+        public string name { get; set; }
+        public decimal price { get; set; }
+        public int stockQuantity { get; set; }
+        public int supplierId { get; set; }
+    }
+}
